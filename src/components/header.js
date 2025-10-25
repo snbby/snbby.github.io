e18ef2564caa5151a63bad1c34fe18fe5acad73c
Max Malyshev <snobby.obnx@gmail.com>
2025-10-25 08:18:49 +0200

    Title: Web Software Engineer

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,6 @@ import Box from '@mui/material/Box';
 export default function Header(){
     return (
         <Box sx={{display:'flex', flexDirection: 'column', justifyContent:'center', alignItems:'center'}}>
-            {/* <img src={pandaImg} alt='Maxim Malyshev' className='profileImage'/> */}
             <Box
                 component='img'
                 src={pandaImg}
@@ -13,7 +12,7 @@ export default function Header(){
                 sx={{ width: 150, height: 150, borderRadius: '50%', marginTop: '32px', marginBottom: '20px'}}
             />
             <Typography variant='h4' component='h1' gutterBottom>Maxim Malyshev</Typography>
-            <Typography variant='h5' component='h2' gutterBottom>Software Engineer</Typography>
+            <Typography variant='h5' component='h2' gutterBottom>Web Software Engineer</Typography>
         </Box>
     );
 }
